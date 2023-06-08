2f4a7485997dc77da3a20efb6b80e5491d85e56c
souravraha <souravraha@ufl.edu>
2023-06-08 16:08:09 +0530

    introduced classcounts

--- src/datamodule.py
+++ src/datamodule.py
@@ -1,15 +1,15 @@
 from random import shuffle
 
 import lightning as L
-import os
+import torch
 from torch.utils.data import DataLoader, Subset
 from torchvision import datasets as D
 from torchvision import transforms as T
 
+
 # Define the LightningDataModules
 class SUN397DataModule(L.LightningDataModule):
     name = "sun397"
-    NUM_CLASSES = 397
     MEANS = [0.4833, 0.4656, 0.4285]
     STDS = [0.2646, 0.2618, 0.2847]
 
@@ -37,30 +37,30 @@ class SUN397DataModule(L.LightningDataModule):
         # Load and split the dataset into train and validation sets
         dataset = D.SUN397(root=self.data_dir, transform=self.transform)
         # Create a list of indices that belong to each class
-        self.cls_idx_list = [[] for _ in range(len(dataset.classes))]
+        cls_idx_list = [[] for _ in range(len(dataset.classes))]
         for i, label in enumerate(dataset._labels):
-            self.cls_idx_list[label].append(i)
+            cls_idx_list[label].append(i)
         
         # Create val and test splits in a stratified manner
         val_idxs = []
         test_idxs = []
-        for idx_list in self.cls_idx_list:
+        for i, idx_list in enumerate(cls_idx_list):
             shuffle(idx_list)
             val_idxs.extend(idx_list[:self.split["val"]])
             test_idxs.extend(idx_list[self.split["val"]:sum(self.split.values())])
+            cls_idx_list[i] = len(idx_list) - sum(self.split.values())
         
+        # Tensor containing num of samples per class
+        self.class_counts = torch.tensor(cls_idx_list)
         # The remaining are the train split
         train_idxs = list(set(range(len(dataset))) - set(val_idxs) - set(test_idxs))
         # Define subsets as required by the stage
-        if stage == "fit":
+        if stage in ["fit", None]:
             self.val_set = Subset(dataset=dataset, indices=val_idxs)
             self.train_set = Subset(dataset=dataset, indices=train_idxs)
         
-        elif stage == "test":
+        if stage in ["test", None]:
             self.test_set = Subset(dataset=dataset, indices=test_idxs)
-        
-        else:
-            raise NotImplementedError()
         
     def train_dataloader(self):
         return DataLoader(self.train_set, batch_size=self.bs, shuffle=True, num_workers=self.nw, pin_memory=True)
